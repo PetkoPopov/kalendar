e213bd3e55f4a44df3d005ee3e9e2e91e32e9358
pipjivko <pipjivko@gmail.com>
2025-02-17 19:00:24 +0200

    almost done

--- makeTable.js
+++ makeTable.js
@@ -1,14 +1,36 @@
-import {count, firstDay} from'./firstDay.js'
-
+import { count } from './firstDay.js'
+import { month } from './month.js'
 function makeTable() {
     console.log('in make table')
 
-    var table = document.createElement('table')
     var showKalendar = document.getElementById('show-kalendar')
+    let tableErase = document.getElementsByTagName('table')[0]
+    if (tableErase) {
+        showKalendar.removeChild(tableErase)
+    }
+    var table = document.createElement('table')
 
+    var thMonth = document.createElement('th')
+    var trMonth = document.createElement('tr')
+    thMonth.setAttribute('colspan', 7)
+    thMonth.textContent = month.textContent
+    trMonth.appendChild(thMonth)
+    table.appendChild(trMonth)
     let trTh = document.createElement('tr')
+
+    var colEmpty = document.createElement('col')
+    colEmpty.setAttribute('span', 5)
+
+    var colWikend = document.createElement('col')
+    colWikend.setAttribute('span', 2)
+    colWikend.setAttribute('style','background-color:lightgreen')
+    table.appendChild(colEmpty)
+    table.appendChild(colWikend)
+
+
     for (let d = 1; d <= 7; d++) {
         var th = document.createElement('th')
+
         if (d == 1) {
             th.textContent = 'Понеделник'
         } else if (d == 2) {
@@ -31,14 +53,15 @@ function makeTable() {
         let tr = document.createElement('tr')
         for (let d = 1; d <= 7; d++) {
             let td = document.createElement('td')
-            td.textContent=row*d + count
+            td.textContent = row * d + count
             tr.appendChild(td)
         }
         table.appendChild(tr)
     }
-    // table.appendChild(tr)
 
     showKalendar.appendChild(table)
+    let clear = document.getElementById('clear')
+    clear.style.display = 'none'
 }
 
 export {
